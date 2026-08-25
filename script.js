528bbe3e128c00efcf68f99ecb04feff6e9ebe6c
tharadonac52-ship-it <tharadon.ac52@gmail.com>
2026-08-25 09:11:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใล้ (Optional)
 document.addEventListener('DOMContentLoaded',function () {
    document.getElementById('year').innerHTML =new Date().getFullYear();
    console.log('Portfolio ของ SOMCHAI พร้อมแล้ว!');
